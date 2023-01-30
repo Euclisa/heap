c194cf68b1ecb5d2858e6983aa4457ff1e423f1e
Matvey Klochihin <matveiklochihin@gmail.com>
2023-01-30 15:04:37 +0300

    All methods are now scoped. All service functions are hidden.

--- heap.h
+++ heap.h
@@ -22,14 +22,14 @@
 
 #define hp_size(heap) heap[0]
 
-#define hp_in_heap(heap,i) ((i <= hp_size(heap)) && (i > 0))
+#define __hp_in_heap(heap,i) ((i <= hp_size(heap)) && (i > 0))
 
-#define hp_left_child(i) (2*i)
-#define hp_right_child(i) (2*i+1)
-#define hp_parent(i) (i/2)
-#define hp_root 1
+#define __hp_left_child(i) (2*i)
+#define __hp_right_child(i) (2*i+1)
+#define __hp_parent(i) (i/2)
+#define __hp_root 1
 
-#define hp_swap(heap,i,j)                                                                                       \
+#define __hp_swap(heap,i,j)                                                                                     \
             heap[i] = heap[i] + heap[j];                                                                        \
             heap[j] = heap[i] - heap[j];                                                                        \
             heap[i] = heap[i] - heap[j];
@@ -42,18 +42,18 @@
     long i = ind;                                                                                               \
     ret = HP_SUCCESS;                                                                                           \
     if(hp_empty(heap)) ret = HP_HEAP_EMPTY;                                                                     \
-    if(!hp_in_heap(heap,i) && ret == HP_SUCCESS) ret = HP_INDEX_OUT_OF_RANGE;                                   \
+    if(!__hp_in_heap(heap,i) && ret == HP_SUCCESS) ret = HP_INDEX_OUT_OF_RANGE;                                 \
     if(ret == HP_SUCCESS)                                                                                       \
         while(1)                                                                                                \
         {                                                                                                       \
             long largest = i;                                                                                   \
-            if(hp_in_heap(heap,hp_left_child(i)) && hp_compare(heap[hp_left_child(i)],heap[largest]))           \
-                largest = hp_left_child(i);                                                                     \
-            if(hp_in_heap(heap,hp_right_child(i)) && hp_compare(heap[hp_right_child(i)],heap[largest]))         \
-                largest = hp_right_child(i);                                                                    \
+            if(__hp_in_heap(heap,__hp_left_child(i)) && hp_compare(heap[__hp_left_child(i)],heap[largest]))     \
+                largest = __hp_left_child(i);                                                                   \
+            if(__hp_in_heap(heap,__hp_right_child(i)) && hp_compare(heap[__hp_right_child(i)],heap[largest]))   \
+                largest = __hp_right_child(i);                                                                  \
             if(largest != i)                                                                                    \
             {                                                                                                   \
-                hp_swap(heap,largest,i)                                                                         \
+                __hp_swap(heap,largest,i)                                                                       \
                 i = largest;                                                                                    \
             }                                                                                                   \
             else                                                                                                \
@@ -66,38 +66,40 @@
 {                                                                                                               \
     long i = ind;                                                                                               \
     ret = HP_SUCCESS;                                                                                           \
-    if(!hp_in_heap(heap,i)) ret = HP_INDEX_OUT_OF_RANGE;                                                        \
+    if(!__hp_in_heap(heap,i)) ret = HP_INDEX_OUT_OF_RANGE;                                                      \
     if(hp_compare(heap[i],key) && i != (hp_size(heap)) && ret == HP_SUCCESS) ret = HP_INVALID_KEY;              \
     if(ret == HP_SUCCESS)                                                                                       \
     {                                                                                                           \
         heap[i] = key;                                                                                          \
-        while(i > hp_root && hp_compare(heap[i],heap[hp_parent(i)]))                                        \
+        while(i > __hp_root && hp_compare(heap[i],heap[__hp_parent(i)]))                                        \
         {                                                                                                       \
-            hp_swap(heap,i,hp_parent(i))                                                                        \
-            i = hp_parent(i);                                                                                   \
+            __hp_swap(heap,i,__hp_parent(i))                                                                    \
+            i = __hp_parent(i);                                                                                 \
         }                                                                                                       \
     }                                                                                                           \
 }
 
 // Inserts new element with value=key
 #define hp_insert(heap,key,ret)                                                                                 \
+{                                                                                                               \
     hp_size(heap)++;                                                                                            \
     heap[hp_size(heap)] = key;                                                                                  \
-    hp_change_key(heap,hp_size(heap),key,ret);
+    hp_change_key(heap,hp_size(heap),key,ret);                                                                  \
+}
 
 // Removes max (MAX_HEAP) or min (MIN_HEAP) element from heap and puts it in 'out'
 #define hp_pop(heap,out,ret)                                                                                    \
+{                                                                                                               \
     ret = HP_SUCCESS;                                                                                           \
     if(hp_empty(heap)) ret = HP_HEAP_EMPTY;                                                                     \
     if(ret == HP_SUCCESS)                                                                                       \
     {                                                                                                           \
-        out = heap[hp_root];                                                                                \
-        heap[hp_root] = heap[hp_size(heap)];                                                                \
+        out = heap[__hp_root];                                                                                  \
+        heap[__hp_root] = heap[hp_size(heap)];                                                                  \
         hp_size(heap)--;                                                                                        \
         if(!hp_empty(heap))                                                                                     \
-        {                                                                                                       \
-            hp_heapify(heap,hp_root,ret);                                                                   \
-        }                                                                                                       \
-    }
+            hp_heapify(heap,__hp_root,ret);                                                                     \
+    }                                                                                                           \
+}
 
 #endif
